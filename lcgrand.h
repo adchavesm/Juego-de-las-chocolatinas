8f30b25de24e6d7868f05b90528acf5dc7c74fb5
Andres Chaves <adchavesm>
2020-06-26 23:38:33 -0500

    Codigo sin distribuciones

--- lcgrand.h
+++
@@ -1,9 +0,0 @@
-#ifndef LCGRAND_H_INCLUDED
-#define LCGRAND_H_INCLUDED
-
-float lcgrand(int stream);
-void  lcgrandst(long zset, int stream);
-long  lcgrandgt(int stream);
-
-
-#endif // LCGRAND_H_INCLUDED
